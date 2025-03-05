59f4e7720f0a1079f6122b75e6cf8c711083e991
ShubhamKumarRaman <shubham620409@gmail.com>
2025-03-05 23:13:26 +0530

    Update J01Swapping.java

--- 01BasicPrograms/J01Swapping.java
+++ 01BasicPrograms/J01Swapping.java
@@ -1,6 +1,7 @@
 public class J01Swapping {
     // Approach 1: Swapping the Values Using Third Variable
     public static void usingtemp(int num1, int num2) {
+        System.out.println("Using temporary variable:- ");
         System.out.println("Before swapping:- num1:- " + num1 + "  num2:- " + num2);
         int temp = num1;
         num1 = num2;
@@ -8,9 +9,21 @@ public class J01Swapping {
         System.out.println("After swapping:- num1:- " + num1 + "  num2:- " + num2);
     }
 
+    // Approach 2: Swapping the Values Without Using Third Variable by using sum and
+    // differences concepts of math.
+    public static void usingSumDifference(int num1, int num2) {
+        System.out.println("Using Sum and Difference concept:- ");
+        System.out.println("Before swapping:- num1:- " + num1 + "  num2:- " + num2);
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+        System.out.println("After swapping:- num1:- " + num1 + "  num2:- " + num2);
+    }
+
     public static void main(String args[]) {
         int num1 = 5;
         int num2 = 10;
         usingtemp(num1, num2);
+        usingSumDifference(num1, num2);
     }
 }
