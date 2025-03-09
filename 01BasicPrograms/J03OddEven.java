3fa65961854aa861d43af7a706d7cd8fd8ba15ef
ShubhamKumarRaman <shubham620409@gmail.com>
2025-03-09 20:33:21 +0530

    Update J03OddEven.java

--- 01BasicPrograms/J03OddEven.java
+++ 01BasicPrograms/J03OddEven.java
@@ -2,13 +2,23 @@ public class J03OddEven {
     // Brute Force Naive Approach
     public static void usingifelse(int num) {
         if (num % 2 == 0) {
-            System.out.println("Even Number");
+            System.out.println(num + ": Even Number");
         } else {
-            System.out.println("Odd Number");
+            System.out.println(num + ": Odd Number");
+        }
+    }
+
+    // Using Bitwise OR
+    public static void usingBitwiseOR(int num) {
+        if ((num | 1) > num) {
+            System.out.println(num + ": Even Number");
+        } else {
+            System.out.println(num + ": Odd Number");
         }
     }
 
     public static void main(String args[]) {
         usingifelse(10);
+        usingBitwiseOR(13);
     }
 }
