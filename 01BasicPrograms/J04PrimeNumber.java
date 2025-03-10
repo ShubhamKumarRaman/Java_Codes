027e93237761ef4a26b3a9b2564d5994de859fd1
ShubhamKumarRaman <shubham620409@gmail.com>
2025-03-10 22:28:21 +0530

    Update J04PrimeNumber.java

--- 01BasicPrograms/J04PrimeNumber.java
+++ 01BasicPrograms/J04PrimeNumber.java
@@ -35,8 +35,40 @@ public class J04PrimeNumber {
         }
     }
 
+    public static void printPrime2(int N)
+    {
+        // Declaring the variables
+        int x, y, flg;
+
+        // Printing display message
+        System.out.println(
+            "All the Prime numbers within 1 and " + N
+            + " are:");
+
+        // Using for loop for traversing all
+        // the numbers from 1 to N
+        for (x = 2; x <= N; x++) {
+
+            // Using flag variable to check
+            // if x is prime or not
+            flg = 1;
+
+            for (y = 2; y * y <= x; y++) {
+                if (x % y == 0) {
+                    flg = 0;
+                    break;
+                }
+            }
+
+            // If flag is 1 then x is prime but
+            // if flag is 0 then x is not prime
+            if (flg == 1)
+                System.out.print(x + " ");
+    }
+
     public static void main(String args[]) {
         printPrime(50);
+        printPrime2(60);
 
     }
 }
