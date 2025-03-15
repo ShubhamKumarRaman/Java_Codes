04c8765238bf9131cb76eecc3db82817a4a167aa
ShubhamKumarRaman <shubham620409@gmail.com>
2025-03-15 22:26:58 +0530

    Update J06LeapYear.java

--- 01BasicPrograms/J06LeapYear.java
+++ 01BasicPrograms/J06LeapYear.java
@@ -1,4 +1,5 @@
 public class J06LeapYear {
+    // Using Nested if-else
     public static void usingNested(int year) {
         boolean isLeapYear = false;
         if (year % 400 == 100) {
@@ -19,8 +20,19 @@ public class J06LeapYear {
         }
     }
 
+    // Using Logical Operator
+    public static void usingLogicalOp(int year) {
+        if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) {
+            System.out.println(year + ": Leap Year.");
+        } else {
+            System.out.println(year + ": Not a leap year.");
+        }
+    }
+
     public static void main(String args[]) {
         usingNested(1900);
         usingNested(2012);
+        usingLogicalOp(2021);
+        usingLogicalOp(1892);
     }
 }
