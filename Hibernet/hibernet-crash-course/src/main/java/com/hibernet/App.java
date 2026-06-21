dc63a6d88825a01e9b4542d6da26d7dc539214e4
Shubham Kumar <shubham620409@gmail.com>
2026-06-21 22:08:31 +0530

    Update App.java

--- Hibernet/hibernet-crash-course/src/main/java/com/hibernet/App.java
+++ Hibernet/hibernet-crash-course/src/main/java/com/hibernet/App.java
@@ -1,13 +1,10 @@
 package com.hibernet;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("We are going to learn hibernet.");
     }
 }
